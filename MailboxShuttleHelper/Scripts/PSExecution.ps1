713f6e95269b7412de574b23daf547ee779f5f64
Carpmenter <nickcarpenter1017@gmail.com>
2019-09-21 11:12:37 -0500

    Added variables and button click event

--- MailboxShuttleHelper/Scripts/PSExecution.ps1
+++ MailboxShuttleHelper/Scripts/PSExecution.ps1
@@ -1,8 +1,4 @@
-#create window
-
-$xamlFile = "C:\Users\nick.carpenter\source\repos\MailboxShuttleHelper\MailboxShuttleHelper\MainWindow.xaml"
-
-$inputXML = Get-Content $xamlFile -Raw
+Add-Type -AssemblyName PresentationFramework
 
 Function Connect-Tenant {
 
@@ -16,7 +12,7 @@ Function Connect-Tenant {
 Function Check-MoveRequest {
     Param($Mailbox)
 
-    Get-MoveRequest $Mailbox | Get-MoveRequestStatistics
+    Get-MoveRequest $Mailbox 
 
 }
 
@@ -26,3 +22,41 @@ Function Check-Mailbox {
     Get-Mailbox $Mailbox | fl
 
 }
+
+
+#create window
+
+$xamlFile = "C:\Users\nick.carpenter\source\repos\MailboxShuttleHelper\MailboxShuttleHelper\MainWindow.xaml"
+
+$inputXML = Get-Content $xamlFile -Raw
+$inputXML = $inputXML -replace 'mc:Ignorable="d"', '' -replace "x:N", 'N' -replace '^<Win.*', '<Window'
+[XML]$XAML = $inputXML
+
+#Read XAML
+$reader = (New-Object System.Xml.XmlNodeReader $xaml)
+try {
+    $window = [Windows.Markup.XamlReader]::Load( $reader )
+} catch {
+    Write-Warning $_.Exception
+    throw
+}
+
+$xaml.SelectNodes("//*[@Name]") | ForEach-Object {
+    #"trying item $($_.Name)"
+    try {
+        Set-Variable -Name "var_$($_.Name)" -Value $window.FindName($_.Name) -ErrorAction Stop
+    } catch {
+        throw
+    }
+}
+
+$var_mbxSearch.Add_Click( {
+    #Clear textbox
+    #$var_mbxInput.Text = ""
+        if ($result = Check-MoveRequest -Mailbox $var_mbxInput.Text) {
+            $var_txtResults.Text = $result
+        }
+})
+
+
+$Null = $window.ShowDialog()
