c3988dc5de3d3bf312ad4f4758acc53f53a3b9b9
Carpmenter <nickcarpenter1017@gmail.com>
2019-09-20 22:13:29 -0500

    Adding powershell functions

--- MailboxShuttleHelper/Scripts/PSExecution.ps1
+++ MailboxShuttleHelper/Scripts/PSExecution.ps1
@@ -3,3 +3,26 @@
 $xamlFile = "C:\Users\nick.carpenter\source\repos\MailboxShuttleHelper\MailboxShuttleHelper\MainWindow.xaml"
 
 $inputXML = Get-Content $xamlFile -Raw
+
+Function Connect-Tenant {
+
+    $UserCredential = Get-Credential
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+
+    Import-PSSession $Session
+}
+
+
+Function Check-MoveRequest {
+    Param($Mailbox)
+
+    Get-MoveRequest $Mailbox | Get-MoveRequestStatistics
+
+}
+
+Function Check-Mailbox {
+    Param($Mailbox)
+
+    Get-Mailbox $Mailbox | fl
+
+}
